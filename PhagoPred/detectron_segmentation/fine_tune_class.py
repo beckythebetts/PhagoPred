263156c85628811dfad3aeb99efda22ecf78ad59
Ubuntu <ubuntu@ip-172-19-196-177.eu-west-2.compute.internal>
2025-08-19 13:46:48 +0000

    Fixing fine tuning

--- PhagoPred/detectron_segmentation/fine_tune_class.py
+++ PhagoPred/detectron_segmentation/fine_tune_class.py
@@ -47,6 +47,13 @@ import pandas as pd
 import h5py
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import torch
+import torch.nn.functional as F
+
+from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputLayers
+from detectron2.structures import Boxes, Instances, pairwise_iou
+from detectron2.modeling.box_regression import Box2BoxTransform
+from detectron2.layers import cat
 
 from PhagoPred.utils import tools, mask_funcs
 from PhagoPred.detectron_segmentation import train
@@ -54,14 +61,22 @@ from PhagoPred import SETTINGS
 
 
 import random
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def get_finetuning_dataset(hdf5_file: Path = SETTINGS.DATASET, model_dir: Path = SETTINGS.MASK_RCNN_MODEL):
+def get_finetuning_dataset(
+        hdf5_file: Path = SETTINGS.DATASET, 
+        model_dir: Path = SETTINGS.MASK_RCNN_MODEL,
+        include_no_cell: bool = True,
+        ):
     """For each cell in death_frames, take frames before and after cell death. 
     Crop images/masks to minimum required to cover cell.
-    Split into train/val folders based on Cell Idx (~80/20).
+    Split into train/val folders based on Cell Idx (~80:20).
+    If include_no_cell is true, take random crops of areas with no cell.
     """
     base_dir = Path(model_dir) / 'Fine_Tuning_Data'
     tools.remake_dir(base_dir)
+
+    crop_shapes = {'train': [], 'validate': []}
 
     # Define directories for train and val
     dirs = {}
@@ -100,13 +115,15 @@ def get_finetuning_dataset(hdf5_file: Path = SETTINGS.DATASET, model_dir: Path =
     annotation_id = 1
     image_id = 1
 
-    pbar = tqdm(total=len(death_frames) * len(frames_sample) * 2)
+    pbar = tqdm(total=len(death_frames) * len(frames_sample) * 3)
 
     with h5py.File(hdf5_file, 'r') as f:
         for cell_idx, death_frame in zip(death_frames['Cell Idx'], death_frames['Death Frame']):
             split = 'validate' if cell_idx in val_cell_ids else 'train'
             coco_json = dirs[split]['coco_json']
             images_dir = dirs[split]['images']
+
+            num_frames, image_h, image_w = f['Images']['Phase'].shape
 
             # Alive frames
             for alive_frame in death_frame - frames_sample:
@@ -122,6 +139,8 @@ def get_finetuning_dataset(hdf5_file: Path = SETTINGS.DATASET, model_dir: Path =
                 x_coords, y_coords = np.nonzero(mask)
                 x_crop = slice(np.min(x_coords), np.max(x_coords) + 1)
                 y_crop = slice(np.min(y_coords), np.max(y_coords) + 1)
+
+                crop_shapes[split].append((x_crop.stop - x_crop.start, y_crop.stop - y_crop.start))
 
                 image = f['Images']['Phase'][alive_frame]
                 image = image[x_crop, y_crop]
@@ -151,6 +170,8 @@ def get_finetuning_dataset(hdf5_file: Path = SETTINGS.DATASET, model_dir: Path =
                 x_crop = slice(np.min(x_coords), np.max(x_coords) + 1)
                 y_crop = slice(np.min(y_coords), np.max(y_coords) + 1)
 
+                crop_shapes[split].append((x_crop.stop - x_crop.start, y_crop.stop - y_crop.start))
+
                 image = f['Images']['Phase'][dead_frame][x_crop, y_crop]
                 mask = mask[x_crop, y_crop]
 
@@ -164,6 +185,51 @@ def get_finetuning_dataset(hdf5_file: Path = SETTINGS.DATASET, model_dir: Path =
                 image_id += 1
                 pbar.update(1)
 
+        if include_no_cell:
+            for split in ['train', 'validate']:
+                crops = crop_shapes[split]
+
+                coco_json = dirs[split]['coco_json']
+                images_dir = dirs[split]['images']
+
+                num_samples = len(crops) / 2 # Get approximately same number of no cell images as alive/dead cell images
+                attempts = 0
+                added = 0
+                max_attempts = len(crops) * 20  # avoid infinite loop   
+
+                coco_json = dirs[split]['coco_json'] 
+
+                while added < num_samples and attempts < max_attempts:
+                    attempts += 1
+                    frame = random.randint(0, num_frames - 1)           
+
+                    h_crop, w_crop = random.choice(crops)
+
+                    if h_crop >= image_h or w_crop >= image_w:
+                        continue
+
+                    x_0 = random.randint(0, image_h - h_crop)
+                    y_0 = random.randint(0, image_w - w_crop)
+
+                    mask_crop = f['Segmentations']['Phase'][frame][x_0:x_0+h_crop, y_0:y_0+w_crop]
+                    if (mask_crop >= 0).any():
+                        continue
+
+                    image = f['Images']['Phase'][frame][x_0:x_0+h_crop, y_0:y_0+w_crop]
+                    image_file = f'no_cell_{added}.jpeg'
+
+                    plt.imsave(images_dir / image_file, image / 255, cmap='gray')
+                    coco_json["images"].append({
+                        "id": image_id,
+                        "width": image.shape[1],
+                        "height": image.shape[0],
+                        "file_name": image_file,
+                        })
+                    
+                    image_id += 1
+                    added += 1
+                    pbar.update(1)
+
     pbar.close()
 
     # Save COCO jsons
@@ -171,24 +237,90 @@ def get_finetuning_dataset(hdf5_file: Path = SETTINGS.DATASET, model_dir: Path =
         with open(dirs[split]['json_file'], 'w') as f:
             json.dump(dirs[split]['coco_json'], f)
 
-            
+# class ClassOnlyFastRCNNOutputLayers(FastRCNNOutputLayers):
+#     def __init__(self, input_shape, *, box2box_transform, num_classes, **kwargs):
+#         super().__init__(
+#             input_shape,
+#             box2box_transform=box2box_transform,
+#             num_classes=num_classes,
+#             **kwargs
+#         )
+    # def losses(self, predictions, proposals):
+    #     """
+    #     Override loss to ignore background class and optionally ignore box regression loss.
+    #     """
+    #     scores, proposal_deltas = predictions
+    #     device = scores.device
+
+    #     # Concatenate ground truth classes from proposals
+    #     gt_classes = cat([p.gt_classes for p in proposals], dim=0) if len(proposals) else torch.empty(0, dtype=torch.int64, device=device)
+
+    #     # Filter out background (class == num_classes)
+    #     is_foreground = gt_classes < self.num_classes
+    #     scores_fg = scores[is_foreground]
+    #     gt_classes_fg = gt_classes[is_foreground]
+
+    #     if len(gt_classes_fg) == 0:
+    #         # no foreground samples in this batch: return zero loss
+    #         loss_cls = torch.tensor(0.0, device=device, requires_grad=True)
+    #     else:
+    #         loss_cls = F.cross_entropy(scores_fg, gt_classes_fg)
+
+    #     # Option 1: ignore box regression loss completely
+    #     loss_box_reg = torch.tensor(0.0, device=device, requires_grad=True)
+
+    #     # Option 2: If you want to keep box loss, uncomment below and comment Option 1 above
+    #     # proposal_boxes = [p.proposal_boxes for p in proposals]
+    #     # gt_boxes = [p.gt_boxes for p in proposals]
+    #     # loss_box_reg = self.box_reg_loss(proposal_deltas, proposal_boxes, gt_boxes, gt_classes)
+        
+    #     print(f"scores device: {scores.device}")
+    #     print(f"proposal_deltas device: {proposal_deltas.device}")
+    #     for p in proposals:
+    #         print(f"proposal gt_classes device: {p.gt_classes.device}")
+
+    #     return {
+    #         "loss_cls": loss_cls,
+    #         "loss_box_reg": loss_box_reg,
+    #     }     
+    
 class ClassifierHeadFineTuner(train.MyTrainer):
     """Freeze everything except classification head, and remove all augemtnations in mapper (can't set augmentation sizes to image size like in MyTrainer as image crop sizes vary)."""
-    @classmethod
-    def build_optimizer(cls, cfg, model):
-        """Freezes eveyhting but classificaiton head."""
-        for name, param in model.named_parameters():
-            # if "roi_heads.box_predictor.cls_score" in name:
-            if "roi_heads.box_predictor" in name:
-                param.requires_grad = True
+    def build_hooks(self):
+        hooks = super().build_hooks()
+        hooks.insert(-1, train.LossEvalHook(
+            self.cfg.TEST.EVAL_PERIOD,
+            self.model,
+            build_detection_test_loader(
+                self.cfg,
+                self.cfg.DATASETS.TEST[0],
+                # DatasetMapper(self.cfg, True),
+                mapper = no_resize_mapper,
+            )
+        ))
+        return hooks
+    
+    # @classmethod
+    # def build_optimizer(cls, cfg, model):
+    #     """Freezes eveyhting but classificaiton head."""
+    #     for name, param in model.named_parameters():
+    #         if "roi_heads.box_predictor.cls_score" in name:
+    #         # if "roi_heads.box_predictor" in name:
+    #             param.requires_grad = True
 
-            else:
-                param.requires_grad = False
+    #         else:
+    #             param.requires_grad = False
         
-        return torch.optim.Adam(
-            [p for p in model.parameters() if p.requires_grad],
-            lr=cfg.SOLVER.BASE_LR
-        )
+    #     return torch.optim.Adam(
+    #         [p for p in model.parameters() if p.requires_grad],
+    #         lr=cfg.SOLVER.BASE_LR
+    #     )
+    
+    # @classmethod
+    # def build_model(cls, cfg):
+    #     model = super().build_model(cfg)
+    #     replace_box_predictor_with_custom(model, cfg)
+    #     return model
     
     def build_train_loader(self, cfg):
         """Set mapper as no resize mapper,"""
@@ -203,28 +335,65 @@ def no_resize_mapper(dataset_dict):
 
     # Load image from file
     image = utils.read_image(dataset_dict["file_name"], format="BGR")
-    image = torch.as_tensor(image.transpose(2, 0, 1).copy())
-    dataset_dict["image"] = image
+    # image = torch.as_tensor(image.transpose(2, 0, 1).copy())
+    # dataset_dict["image"] = image
 
+    # print(f"\nImage shape: {image.shape}\n")
+
+    augs = T.AugmentationList([T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
+                               T.RandomFlip(prob=0.5, horizontal=False, vertical=True)])
+    
+    aug_input = T.AugInput(image)
+
+    transforms = augs(aug_input)
+    image = torch.as_tensor(aug_input.image.transpose(2, 0, 1).copy())
+    dataset_dict["image"] = image
+    # image, transforms = T.apply_augmentations(augs, image)
     # Load annotations (if training)
     if "annotations" in dataset_dict:
         annos = [
-            utils.transform_instance_annotations(obj, [], image.shape[:2])
+            utils.transform_instance_annotations(obj, transforms, image.shape[:2])
             for obj in dataset_dict.pop("annotations")
         ]
         dataset_dict["instances"] = utils.annotations_to_instances(annos, image.shape[:2])
 
     return dataset_dict
 
+# def replace_box_predictor_with_custom(model, cfg):
+#     box2box_transform = model.roi_heads.box_predictor.box2box_transform
+
+#     model.roi_heads.box_predictor = ClassOnlyFastRCNNOutputLayers(
+#         model.roi_heads.box_head.output_shape,
+#         box2box_transform=box2box_transform,
+#         num_classes=cfg.MODEL.ROI_HEADS.NUM_CLASSES,
+#         cls_agnostic_bbox_reg=cfg.MODEL.ROI_BOX_HEAD.CLS_AGNOSTIC_BBOX_REG,
+#         test_score_thresh=0.0,  # optionally set or leave default
+#         test_nms_thresh=0.5,
+#         test_topk_per_image=100,
+#         smooth_l1_beta=0.0,
+#         box_reg_loss_type="smooth_l1",
+#     )
+#     device = next(model.parameters()).device
+#     model.roi_heads.box_predictor.to(device)
+#     print("Box head output shape:", model.roi_heads.box_head.output_shape)
+#     print("Box predictor device:", next(model.roi_heads.box_predictor.parameters()).device)
+#     print("Box head parameters device:", next(model.roi_heads.box_head.parameters()).device)
+
+#     pass
 
 
-def train(directory=SETTINGS.MASK_RCNN_MODEL):
+
+def fine_tune(directory=SETTINGS.MASK_RCNN_MODEL):
     """
     Fine tune the Mask R-CNNmodel on cropped cell images just before and after cell death. Freeze everything expect for classification head.
     """
     # if "DiceLSMaskHead" not in ROI_MASK_HEAD_REGISTRY._obj_map:
     #     ROI_MASK_HEAD_REGISTRY.register(DiceLSMaskHead)
     setup_logger()
+
+    metrics_path = Path(directory / 'Model' / 'metrics.json')
+    if metrics_path.exists():
+        metrics_path.rename(metrics_path.with_name("original_metrics.json"))
 
     dataset_dir = directory / 'Fine_Tuning_Data'
     config_directory = directory / 'Model'
@@ -237,7 +406,8 @@ def train(directory=SETTINGS.MASK_RCNN_MODEL):
     cfg.merge_from_file(str(directory / 'Model' / 'config.yaml'))
     cfg.OUTPUT_DIR = str(directory / 'Model')
     cfg.MODEL.WEIGHTS = str(directory / 'Model' / 'model_final.pth')
-    cfg.SOLVER.BASE_LR = 0.0001 # Decrease learnign rate for fine tuning (was 0.00025 for main training)
+    cfg.SOLVER.BASE_LR = 1e-5 # Decrease learnign rate for fine tuning (was 0.00025 for main training)
+    cfg.SOLVER.IMS_PER_BATCH = 1 # Avoids issues with collating images of different sizes
 
     trainer = ClassifierHeadFineTuner(cfg)
     trainer.resume_or_load(resume=False)
@@ -269,7 +439,11 @@ def plot_loss(cfg_dir):
 
 
 def main():
-    train()
+    # mask_funcs.clean_coco_json(
+    #     Path('PhagoPred/detectron_segmentation/models/27_05_mac_new_bigger_dataset/Fine_Tuning_Data/validate/labels.json'),
+    #     Path('PhagoPred/detectron_segmentation/models/27_05_mac_new_bigger_dataset/Fine_Tuning_Data/validate/images'),
+    #     )
+    fine_tune()
     # get_finetuning_dataset()
     # plot_loss(Path("PhagoPred/detectron_segmentation/models/27_05_mac_finetune/Model"))
 
